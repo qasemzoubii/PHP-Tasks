73c55a03527e5d51dfdcc4a3d8a8602b0dbd3a89
qasemzoubii <qasem.zoubi2000@gmail.com>
2023-08-14 00:19:37 +0300

    modified

---
+++ 13-august-2023/delete.php
@@ -0,0 +1,33 @@
+<?php
+if (isset($_GET["id"])){
+    $id = $_GET['id'];
+
+
+    $servername = "localhost";
+    $username   = "root"; // username database
+    $password   = ""; 	// password database
+    $database   = "myshop";
+
+    // create connection
+    $connection = new mysqli($servername, $username, $password, $database);
+
+    $sql = "DELETE FROM clients WHERE id=$id";
+    $connection->query($sql);
+}
+
+header ("location:index.php");
+exit;
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+?>
